7ca6a7df84f257dc9f5708d090e800242a3c140d
Jahid1604091 <jh409780@gmail.com>
2023-06-26 16:35:00 +0600

    basic cleanup and setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import styled from "styled-components";
+
+export default function App() {
+  return (
+    <Wrapper className="section">
+      <Header />
+      APp
+      <Footer />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section``;
